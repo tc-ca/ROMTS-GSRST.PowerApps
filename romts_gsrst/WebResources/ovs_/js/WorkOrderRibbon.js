dd2f13b0ad9fd4d71d5281ecae0e5181da8878ce
tc-buildbot <nepasrepondre-noreply@tc.gc.ca>
2021-04-08 20:44:58 +0000

    Update Solution - romts_gsrst

---
+++ romts_gsrst/WebResources/ovs_/js/WorkOrderRibbon.js
@@ -0,0 +1,97 @@
+function ActivateWorkOrder(primaryControl) {
+    const formContext = primaryControl;
+
+    var confirmStrings = { confirmButtonLabel: "Activate",  cancelButtonLabel: "Cancel" , text: "Are you sure you want to activate the selected 1 Work Order?\nThis will set the Work Order to the Active state.", title: "Confirm Work Order Activation" }
+    var confirmOptions;
+    Xrm.Navigation.openConfirmDialog(confirmStrings, confirmOptions).then(
+        function (success) {
+            if (success.confirmed){
+                
+                formContext.getAttribute("statecode").setValue(0);
+                formContext.getAttribute("statuscode").setValue(1);
+                formContext.getAttribute("msdyn_systemstatus").setValue(690970003); //Open - Completed
+            
+                openWorkOrderServiceTasks(formContext);
+                openBookableResourceBookings(formContext);
+
+                setWorkOrderServiceTasksView(formContext);
+                setBookableResourceBookingsView(formContext);
+
+                formContext.data.save();
+            }
+
+        },
+        function (error) {
+            console.log(error.message);
+        }
+    );
+}
+
+function openWorkOrderServiceTasks(formContext) {
+    workOrderServiceTaskData = 
+    {
+        "statecode" :  0,           //closed -> 1
+        "statuscode" : 918640002    //closed -> 918640003
+    };
+
+    Xrm.WebApi.online.retrieveMultipleRecords("msdyn_workorderservicetask", `?$select=msdyn_workorder&$filter=msdyn_workorder/msdyn_workorderid eq ${formContext.data.entity.getId()}`).then(
+        function success(result) {
+            for (var i = 0; i < result.entities.length; i++) {
+                Xrm.WebApi.updateRecord("msdyn_workorderservicetask", result.entities[i].msdyn_workorderservicetaskid, workOrderServiceTaskData).then(
+                    function success(result) {
+                    },
+                    function (error) {
+                        console.log(error.message);
+                    }
+                );
+            }
+        },
+        function (error) {
+        }
+    );
+}
+
+function openBookableResourceBookings(formContext) {
+    bookableResourceBookingData = 
+            {
+                "statecode" :  0,           //closed -> 1
+                "statuscode" : 1            //closed -> 2
+            };
+    Xrm.WebApi.online.retrieveMultipleRecords("bookableresourcebooking", `?$select=msdyn_workorder&$filter=msdyn_workorder/msdyn_workorderid eq ${formContext.data.entity.getId()}`).then(
+        function success(result) {
+            for (var i = 0; i < result.entities.length; i++) {
+                Xrm.WebApi.updateRecord("bookableresourcebooking", result.entities[i].bookableresourcebookingid, bookableResourceBookingData).then(
+                    function success(result) {
+                    },
+                    function (error) {
+                        console.log(error.message);
+                    }
+                );
+            }
+        },
+        function (error) {
+        }
+    );
+}
+
+function setWorkOrderServiceTasksView(formContext){
+    var activeWorkOrderServiceTasksView = 
+    {
+        entityType: "savedquery",
+        id: "{C9FD8F4D-8184-4DDB-A31A-89E66E8E710E}",
+        name: "Active Work Order Service Tasks"
+    }
+    
+    formContext.getControl("workorderservicetasksgrid").getViewSelector().setCurrentView(activeWorkOrderServiceTasksView);
+}
+
+function setBookableResourceBookingsView(formContext){
+    var activeBookingsView = 
+    {
+        entityType: "savedquery",
+        id: "{8AF53D0E-07FE-49D4-BBBA-CA524DD6551B}",
+        name: "Active Resource Bookings (Field Service Information)"
+    };   
+
+    formContext.getControl("bookings").getViewSelector().setCurrentView(activeBookingsView);
+}
---
+++ romts_gsrst/WebResources/ovs_/js/surveyRender.js
@@ -0,0 +1,106 @@
+var lang = parent.Xrm.Utility.getGlobalContext().userSettings.languageId;
+
+var CharactersRemaining;
+
+if (lang == 1036) {
+    CharactersRemaining = "caractères restants";
+}
+else {
+    CharactersRemaining = "characters remaining";
+}
+
+'use strict';
+window.parentExecutionContext = null;
+window.parentFormContext = null;
+Survey.StylesManager.applyTheme('default');
+
+function InitialContext(executionContext) {
+    window.parentExecutionContext = executionContext;
+    window.parentFormContext = executionContext.getFormContext();
+}
+
+function InitializeSurveyRender(surveyDefinition, surveyResponse, surveyLocale, mode) {
+
+    if (surveyDefinition == null) {
+        return;
+    }
+
+    var questionnaireDefinition = JSON.parse(surveyDefinition);
+    window.survey = new Survey.Model(questionnaireDefinition);
+    survey.locale = surveyLocale;
+    survey.showCompletedPage = false;
+    survey.mode = mode;
+
+    if (surveyResponse != null) {
+        survey.data = JSON.parse(surveyResponse);
+    }
+
+    survey.onComplete.add(function (survey, options) {
+        // When survey is completed, parse the resulting JSON and save it to ovs_questionnaireresponse
+        var data = JSON.stringify(survey.data, null, 3);
+        window.parentFormContext.getAttribute('ovs_questionnaireresponse').setValue(data.trim());
+
+        // In order to keep the survey in place without showing a thank you or blank page
+        // Set the state to running, keep the data and go to the first page
+        survey.clear(false, true);
+        survey.render();
+    });
+
+    survey.onAfterRenderSurvey.add(function (survey, options) {
+        // Hide complete button after survey renders.
+        $('.sv_complete_btn').hide();
+    });
+
+    survey.onValueChanging.add(function (survey, options) {
+        //Adding a space to the questionnaireresponse to make the form dirty. The space gets trimmed off in survey.onComplete.
+        var data = JSON.stringify(survey.data, null, 3) + " ";
+        window.parentFormContext.getAttribute('ovs_questionnaireresponse').setValue(data);
+    });
+
+    survey.onValueChanged.add(function (survey, options) {
+        const el = document.getElementById(options.name);
+        if (el) {
+            el.value = options.value;
+        }
+    });
+
+    //Create showdown markdown converter
+    var converter = new showdown.Converter();
+    survey.onTextMarkdown.add(function (survey, options) {
+        //convert the markdown text to html
+        var str = converter.makeHtml(options.text);
+        //remove root paragraphs <p></p>
+        str = str.substring(3);
+        str = str.substring(0, str.length - 4);
+        //set html
+        options.html = str;
+    });
+
+    survey.onAfterRenderQuestion.add(function (survey, options) {
+        if (options.question.getType() !== "comment") return;
+        var comment = options.htmlElement.getElementsByTagName('textarea')[0];
+        var maxLength = options.question.maxLength;
+        if (maxLength !== -1) {
+            comment.setAttribute("maxLength", maxLength);
+            var div = document.createElement("div");
+            div.style.textAlign = "left";
+            comment.parentNode.appendChild(div);
+            var changingHandler = function () {
+                var currLength = comment.value.length;
+                div.innerText = (options.question.maxLength - currLength) + " " + CharactersRemaining;
+            }
+            changingHandler();
+            comment.onkeyup = changingHandler;
+        }
+    });
+
+
+    $('#surveyElement').Survey({
+        model: survey
+    });
+
+}
+
+function DoComplete() {
+    window.survey.doComplete();
+} 
