ac37660a6b1fbc4ec86b31b58ea5891973af029a
tc-buildbot <nepasrepondre-noreply@tc.gc.ca>
2022-07-18 14:10:22 +0000

    Update Solution - romts_gsrst 1.0.20220718.1

---
+++ romts_gsrst/WebResources/ovs_/js/IncompleteInspectionJustification.js
@@ -0,0 +1,17 @@
+﻿"use strict";
+var ROM;
+(function (ROM) {
+    var IncompleteInspectionJustification;
+    (function (IncompleteInspectionJustification) {
+        // EVENTS
+        function onLoad(eContext) {
+            var form = eContext.getFormContext();
+            var warningMessage = Xrm.Utility.getResourceString("ovs_/resx/IncompleteInspectionJustification", "WarningMessageText");
+            form.ui.setFormNotification(warningMessage, "WARNING", "WarningMessage");
+        }
+        IncompleteInspectionJustification.onLoad = onLoad;
+        function onSave(eContext) {
+        }
+        IncompleteInspectionJustification.onSave = onSave;
+    })(IncompleteInspectionJustification = ROM.IncompleteInspectionJustification || (ROM.IncompleteInspectionJustification = {}));
+})(ROM || (ROM = {}));
