30ed872e0118bca0bf3d5751529253902ef6ca17
tc-buildbot <nepasrepondre-noreply@tc.gc.ca>
2022-07-19 16:13:32 +0000

    Update Solution - romts_gsrst 1.0.20220719.3

---
+++ romts_gsrst/WebResources/ts_/js/RiskThreshold.js
@@ -0,0 +1,18 @@
+﻿"use strict";
+var ROM;
+(function (ROM) {
+    var RiskThreshold;
+    (function (RiskThreshold) {
+        // EVENTS
+        function operationFrequencyOnChange(eContext) {
+            try {
+                var form = eContext.getFormContext();
+                var test = "123";
+            }
+            catch (e) {
+                throw new Error(e.Message);
+            }
+        }
+        RiskThreshold.operationFrequencyOnChange = operationFrequencyOnChange;
+    })(RiskThreshold = ROM.RiskThreshold || (ROM.RiskThreshold = {}));
+})(ROM || (ROM = {}));
