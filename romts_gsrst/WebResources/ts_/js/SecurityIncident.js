d9a50d1f2e5b01bb4f88860c158d32ad81df20b6
tc-buildbot <nepasrepondre-noreply@tc.gc.ca>
2022-11-28 21:24:51 +0000

    Update Solution - romts_gsrst 1.0.20221128.1

--- romts_gsrst/WebResources/ts_/js/SecurityIncident.js
+++ romts_gsrst/WebResources/ts_/js/SecurityIncident.js
@@ -7,6 +7,12 @@ var ROM;
             var formContext = eContext.getFormContext();
             if (formContext.ui.getFormType() == 2) {
                 StatusOfRailwayOwnerOnChange(eContext);
+                var mode = formContext.getAttribute("ts_mode");
+                if (mode.getValue() == 717750001 /* IBT */) {
+                    formContext.getControl("ts_bridgeclosure").setVisible(true);
+                    formContext.getControl("ts_damagestoibtproperty").setVisible(true);
+                    formContext.getControl("ts_ibtlocation").setVisible(true);
+                }
             }
         }
         SecurityIncident.onLoad = onLoad;
@@ -43,5 +49,36 @@ var ROM;
             }
         }
         SecurityIncident.arrestsOnChange = arrestsOnChange;
+        function modeOnChange(eContext) {
+            var form = eContext.getFormContext();
+            var mode = form.getAttribute("ts_mode");
+            if (mode.getValue() == 717750001 /* IBT */) {
+                form.getControl("ts_bridgeclosure").setVisible(true);
+                form.getControl("ts_damagestoibtproperty").setVisible(true);
+                //Retrieve IBT locations
+                var ibtLocationFetchXML = [
+                    "<fetch>",
+                    "  <entity name='msdyn_functionallocation'>",
+                    "    <filter>",
+                    "       <condition attribute='ts_mode' operator='eq' value='717750001'/>",
+                    "    </filter>",
+                    "  </entity>",
+                    "</fetch>",
+                ].join("");
+                //Set custom view for Site
+                var viewIBTLocationId = '{73e123a5-2d58-4642-a298-6d7e4edc089e}';
+                var layoutXmlContact = '<grid name="resultset" jump="msdyn_name" select="1" icon="1" preview="1" object="10117"><row name="result" id="msdyn_functionallocationid"><cell name="msdyn_name" width="300" /><cell name="createdon" width="125" /></row></grid>';
+                var viewDisplayName = "Site";
+                form.getControl("ts_site").addCustomView(viewIBTLocationId, "msdyn_functionallocation", viewDisplayName, ibtLocationFetchXML, layoutXmlContact, true);
+                form.getAttribute("ts_site").setValue(null);
+            }
+            else {
+                form.getControl("ts_bridgeclosure").setVisible(false);
+                form.getControl("ts_damagestoibtproperty").setVisible(false);
+                // form.getControl("ts_ibtlocation").setVisible(false); 57f1ece8-04ac-4178-b2bd-cbd292d2ecc4
+                form.getControl("ts_site").setDefaultView("57f1ece8-04ac-4178-b2bd-cbd292d2ecc4");
+            }
+        }
+        SecurityIncident.modeOnChange = modeOnChange;
     })(SecurityIncident = ROM.SecurityIncident || (ROM.SecurityIncident = {}));
 })(ROM || (ROM = {}));
--- romts_gsrst/WebResources/ts_/js/SecurityIncidentRibbon.js
+++ romts_gsrst/WebResources/ts_/js/SecurityIncidentRibbon.js
@@ -1,12 +1,30 @@
+var lang = parent.Xrm.Utility.getGlobalContext().userSettings.languageId;
+
+var missingFieldsInspectionGenerationTitleLocalized;
+var missingFieldsInspectionGenerationTextLocalized;
+var inspectionGeneratedTextLocalized;
+var inspectionGeneratedTitleLocalized;
+
+if (lang == 1036) {
+    missingFieldsInspectionGenerationTitleLocalized = "Champs manquants";
+    missingFieldsInspectionGenerationTextLocalized = "Un des champs suivant est manquant: Intervenant, Site";
+    inspectionGeneratedTextLocalized = "Ordre de travail créé avec succès";
+    inspectionGeneratedTitleLocalized = "Inspection";
+
+} else {
+    missingFieldsInspectionGenerationTitleLocalized = "Missing fields";
+    missingFieldsInspectionGenerationTextLocalized = "One of the following fields is not set: Reporting Company, Site";
+    inspectionGeneratedTextLocalized = "Work order created successfully";
+    inspectionGeneratedTitleLocalized = "Inspection";
+}
+
 function generateInspection(primaryControl) {
     let stakeholder = primaryControl.getAttribute("ts_stakeholder").getValue();
     let site = primaryControl.getAttribute("ts_site").getValue();
 
-    console.log("Stakeholder " + stakeholder[0].id)
-    console.log("Site " + site[0].id)
-
     if(!stakeholder || !site){
-        showAlertDialog("One of the following fields is not set: Reporting Company, Site, Operation", "Missing values");
+        showAlertDialog(missingFieldsInspectionGenerationTextLocalized, missingFieldsInspectionGenerationTitleLocalized);
+        return;
     }
 
     const securityIncidentId = primaryControl.data.entity.getId().slice(1, -1);
@@ -16,59 +34,18 @@ function generateInspection(primaryControl) {
 
 
     //Create new inspection linked to current Security Inspection
-
     var data =
     {
-        "msdyn_ServiceAccount@odata.bind": `/accounts(${(stakeholder[0].id).slice(1, -1)})`,
-        "ts_Site@odata.bind": `/msdyn_functionallocations/(${(site[0].id).slice(1, -1)})`
+        "msdyn_serviceaccount@odata.bind": `/accounts(${(stakeholder[0].id).slice(1, -1)})`,
+        "ts_Site@odata.bind": `/msdyn_functionallocations(${(site[0].id).slice(1, -1)})`,
+        "ts_SecurityIncident@odata.bind": `/ts_securityincidents(${securityIncidentId})`,
     }
 
     Xrm.WebApi.createRecord("msdyn_workorder", data).then(
         function success(result) {
-            showAlertDialog("WO Created", result.id);
-            console.log("WO Created " + result.id)
-
-            // relatedSecurityIncident = [
-            //     {
-            //         entityType: "ts_securityincident",
-            //         id: securityIncidentId
-            //     }
-            // ];
-
-            // const oneToManyAssociateRequest = {
-            //     getMetadata: () => ({
-            //         boundParameter: null,
-            //         parameterTypes: {},
-            //         operationType: 2,
-            //         operationName: "Associate"
-            //     }),
-
-            //     relationship: "ts_securityincident_msdyn_workorder_SecurityIncident",
-
-            //     target: {
-            //         entityType: "msdyn_workorder",
-            //         id: result.id
-            //     },
-
-            //     relatedEntities: relatedSecurityIncident
-            // }
-
-            // Xrm.WebApi.online.execute(oneToManyAssociateRequest).then(
-            //     function(response) {
-            //         if (response.ok) {
-            //             console.log("Status: %s %s", response.status, response.statusText);
-            //             showAlertDialog("OK", "Status:" + response.status + " " + response.statusText);
-            //         }
-            //     }
-            // )
-            // .catch(function(error) {
-            //     console.log(error.message);
-            //     showAlertDialog("Error", "error.message");
-            // });
+            showAlertDialog(inspectionGeneratedTextLocalized, inspectionGeneratedTitleLocalized)
+            primaryControl.ui.tabs.get("tab_4").setFocus();
         },
-        // function (error) {
-        //     showAlertDialog("Error", error.message);
-        // }
     )
 }
 
